13504243a49299449367f006f5631461e5d0b290
Khamidov0430 <nodirhamidov100@gmail.com>
2023-05-17 17:29:52 +0500

    first commit

---
+++ kirish.js
@@ -0,0 +1,12 @@
+// let hemisbtn = document.querySelector(".hemisbutn");
+// let modal_hemis = document.querySelector(".modal_hemis");
+// hemisbtn.addEventListener("click", () => {
+//   modal_hemis.classList.add(".modal_show");
+// });
+
+const hemisbtn = document.querySelector(".hemisbutn");
+const modal_hemis = document.querySelector(".modal_hemis");
+
+function hemisFunction() {
+  modal_hemis.classList.add("modal_show");
+}
